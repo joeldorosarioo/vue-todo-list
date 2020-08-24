2202f1b048e410854c0f30ce8f01db08d8b7cedd
Joel Oliveira <joeldorosarioo@gmail.com>
2020-08-24 15:41:20 -0300

    Create router

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<transition>
+			<router-view />
+		</transition>
+	</div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+	a {
+		text-decoration: inherit;
+	}
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	ul, ol {
+		list-style: none;
+	}
+
+	h1, h2, h3, h4 {
+		font: inherit;
+	}
+
+	input, select, button {
+		font: inherit;
+	}
+
+	html, body {
+		height: 100%;
+		-webkit-user-select: none;
+		-webkit-touch-callout: none;
+		-webkit-tap-highlight-color: transparent;
+	}
+
+	body {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+
+	#app {
+		height: 100%;
+		overflow: hidden;
+	}
 </style>
